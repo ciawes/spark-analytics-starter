31dfec3083607d7bfafacd5bc9a6ee39ebdff3c7
ciawes <ciawesy@gmail.com>
2022-09-18 12:55:25 +0200

    Initial commit
    Signed-off-by: ciawes <ciawesy@gmail.com>

--- BasicsOfJava/com/example/stream/Car.java
+++ BasicsOfJava/com/example/stream/Car.java
@@ -1,6 +1,9 @@
 package BasicsOfJava.com.example.stream;
 
-public class Car {
+import java.util.Comparator;
+import java.util.Objects;
+
+public class Car implements Comparator<Car> {
     public String name;
     public int topSpeed;
     public int price;
@@ -11,6 +14,24 @@ public class Car {
         this.topSpeed = topSpeed;
         this.price = price;
         this.rating = rating;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return name.equals(car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public int compare(Car o1, Car o2) {
+        return o1.name.compareTo(o2.name);
     }
 
     @Override
